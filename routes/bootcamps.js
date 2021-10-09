dbc0095bd228e3f0414fbf730771711bc3b55f02
MoawiaMaw <agwaenanln@gmail.com>
2021-10-09 21:13:33 +0200

    added controller folder to handle routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,25 +1,15 @@
 const express = require('express');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
 const router = express.Router();
 
-//get all
-router.get("/", (req, res) => {
-    res.status(200).send({ success: true, msg: 'get all bootcamps' });
-});
-//get one
-router.get('/:id', (req, res) => {
-    res.status(200).send({ success: true, msg: `show bootcamp ${req.params.id}` });
-});
-//post
-router.post('/', (req, res) => {
-    res.status(201).send({ success: true, msg: `post new bootcamp` });
-});
-//update 
-router.put('/:id', (req, res) => {
-    res.status(200).send({ success: true, msg: `update bootcamp ${req.params.id}` });
-});
-//delete
-router.delete('/:id', (req, res) => {
-    res.status(200).send({ success: true, msg: `delete bootcamp ${req.params.id}` });
-});
+router.route('/')
+    .get(getBootcamps)          //get all
+    .post(createBootcamp);      //post new
 
-module.exports = router;
+router.route('/:id')
+    .get(getBootcamp)           //get one
+    .put(updateBootcamp)        //update
+    .delete(deleteBootcamp);    //delete
+
+
+module.exports = router; 
